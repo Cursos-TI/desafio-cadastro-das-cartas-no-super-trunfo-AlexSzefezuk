1275bab22a251a098ceecbe1fae6ce946000f51d
Alex Szefezuk <92179905+AlexSzefezuk@users.noreply.github.com>
2025-03-16 13:16:17 +0000

    Desafio nivel aventureiro Tema 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,9 +26,9 @@ int main() {
   printf("Digite o estado: ");
   scanf(" %c", &estado);
   printf("Digite o codigo: ");
-  scanf("%2s", codigo);
+  scanf("%s", codigo);
   printf("Digite a cidade: ");
-  scanf("%29s", cidade);
+  scanf("%s", cidade);
   printf("Digite a populacao: ");
   scanf("%d", &populacao);
   printf("Digite a area: ");
@@ -44,9 +44,9 @@ int main() {
   printf("Digite o estado 2: ");
   scanf(" %c", &estado2);
   printf("Digite o codigo 2: ");
-  scanf("%2s", codigo2);
+  scanf("%s", codigo2);
   printf("Digite a cidade 2: ");
-  scanf("%29s", cidade2);
+  scanf("%s", cidade2);
   printf("Digite a populacao 2: ");
   scanf("%d", &populacao2);
   printf("Digite a area 2: ");
@@ -86,15 +86,84 @@ int main() {
   printf("SuperPoder: %.2f\n", superPoder2);
 
   printf("--------------------Comparacao das Cartas--------------------\n");
- 
-  if(pontosTuristicos > pontosTuristicos2) {
-    printf("Carta 1 Venceu\n");
-  } else {
-    printf("Carta 2 Venceu\n");
-  }
 
-  if(pontosTuristicos == pontosTuristicos2) {
-    printf("Empatou\n");
+  int opcao;
+  printf("Escolha o atributo para comparar:\n");
+  printf("1. Populacao\n");
+  printf("2. Area\n");
+  printf("3. PIB\n");
+  printf("4. Pontos Turisticos\n");
+  printf("5. Densidade Populacional\n");
+  printf("6. PIB per Capita\n");
+  printf("7. SuperPoder\n");
+  scanf("%d", &opcao);
+
+  switch(opcao) {
+    case 1:
+      if(populacao > populacao2) {
+        printf("Carta 1 Venceu\n");
+      } else if(populacao < populacao2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    case 2:
+      if(area > area2) {
+        printf("Carta 1 Venceu\n");
+      } else if(area < area2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    case 3:
+      if(pib > pib2) {
+        printf("Carta 1 Venceu\n");
+      } else if(pib < pib2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    case 4:
+      if(pontosTuristicos > pontosTuristicos2) {
+        printf("Carta 1 Venceu\n");
+      } else if(pontosTuristicos < pontosTuristicos2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    case 5:
+      if(densidadePopulacional < densidadePopulacional2) {
+        printf("Carta 1 Venceu\n");
+      } else if(densidadePopulacional > densidadePopulacional2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    case 6:
+      if(pibPerCapita > pibPerCapita2) {
+        printf("Carta 1 Venceu\n");
+      } else if(pibPerCapita < pibPerCapita2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    case 7:
+      if(superPoder > superPoder2) {
+        printf("Carta 1 Venceu\n");
+      } else if(superPoder < superPoder2) {
+        printf("Carta 2 Venceu\n");
+      } else {
+        printf("Empatou\n");
+      }
+      break;
+    default:
+      printf("Opcao invalida\n");
   }
 
   return 0;
